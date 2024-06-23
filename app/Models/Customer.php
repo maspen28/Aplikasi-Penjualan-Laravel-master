2ef7afea175966c661cfaff445d4975e7f2bbc49
Digiponic BS Chemical <digiponic@gmail.com>
2024-06-23 10:54:44 +0700

    first

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,44 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Customer extends Authenticatable
-{
-    use HasFactory;
-    use Notifiable;
+class Customer extends Authenticatable {
+  use HasFactory, Notifiable;
 
-    protected $table = 'customers';
-    protected $fillable = [
-        'username',
-        'email',
-        'password',
-        'phone_number',
-        'address',
-        'citie_id',
-        'district_id',
-    ];
-    protected $hidden = ['password', 'remember_token'];
+  protected $fillable = [
+    'email',
+    'password',
+    'username',
+    'name',
+    'address',
+    'citie_id',
+    'district_id',
+  ];
 
-    public function district()
-    {
-        return $this->belongsTo(District::class);
-    }
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    public function cart(){
-        return $this->hasMany(Cart::class);
-    }
+  // Relationships
+  public function district() {
+    return $this->belongsTo(District::class);
+  }
 
-    public function order(){
-        return $this->hasMany(Order::class);
-    }
+  public function cart() {
+    return $this->hasMany(Cart::class);
+  }
 
-    public function addCartCsid(){
-
-        return Cart::create([
-            'product_id' => $this->id,
-        ]);
-    }
+  public function order() {
+    return $this->hasMany(Order::class);
+  }
 }
